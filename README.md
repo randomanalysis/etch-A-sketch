a6e73ccbe3f1c97400cbedd1862b375d81ab602e
randomanalysis <115797746+randomanalysis@users.noreply.github.com>
2022-11-24 21:23:07 +0000

    Add solution

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # etch-A-sketch
+
+Creating a sketch pad, sized dynamically, which allows the user to 'draw' by moving the mouse over the grid. 
---
+++ main.js
@@ -0,0 +1,45 @@
+const container = document.querySelector('.Grid-Container');
+const gridCells = document.querySelectorAll('.Grid-Cell');
+const gridButton = document.querySelector('#submit-gridsize');
+const gridsize = document.querySelector('#gridsize');
+const colourChoice = document.querySelector('#colourWell');
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
+function createDivs(sideLength) {
+
+    colourChoice.value = "black";
+
+    document.querySelectorAll(".Grid-Cell").forEach(el => el.remove());
+
+    container.style.setProperty('--cols', sideLength);
+
+    for (let i = 1; i <= sideLength * sideLength; i++) {
+        let cell = document.createElement("div")
+        cell.className = "Grid-Cell"
+        cell.addEventListener("mouseenter", function () {
+            if (mouseDown) {
+                this.style.backgroundColor = 'black';
+            }
+        })
+        container.appendChild(cell);
+    }
+}
+
+gridButton.addEventListener('click', () => {
+    createDivs(gridsize.value);
+});
+
+colourChoice.addEventListener("input", () => {
+    const gridCells = document.querySelectorAll('.Grid-Cell');
+    gridCells.forEach((cell) => {
+        cell.addEventListener("mouseenter", function () {
+            if (mouseDown) {
+                this.style.backgroundColor = colourChoice.value;
+            }
+            
+        })
+    });
+});
